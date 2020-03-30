49d407c6b1e8ca7bbcca6bb5f15bcbaf9c10cafc
Drake Wilcox <drakewilcox@gmail.com>
2020-03-30 15:47:04 -0700

    recreate migrations and add Seed Data to Context.cs
    Co-authored-by: Drake Wilcox <drakewilcox@gmail.com>
    Co-authored-by: Geoff Goetz <geoff.goetz503@gmail.com>

--- EpicodusChan/Models/Message.cs
+++ EpicodusChan/Models/Message.cs
@@ -3,8 +3,11 @@ namespace EpicodusChan.Models
   public class Message
   {
       public int MessageId { get; set; }
-      public string Name { get; set; }
-      public int GroupId { get; set; }
+      public string Title { get; set; }
+      public string UserName { get; set; }
+      public string Entry { get; set; }
+      public string Date { get; set; }
+      // public int GroupId { get; set; }
       public string GroupName { get; set; }
   }
 }
