53de2c76383aea972ca9e5c64d7cdb5bf53e65bc
Drake Wilcox <drakewilcox@gmail.com>
2020-03-31 10:25:41 -0700

    add Groups Model
    Co-authored-by: jeremy kale padot <jeremy@padot.us>
    Co-authored-by: Geoff Goetz <geoff.goetz503@gmail.com>

--- EpicodusChan/Models/Message.cs
+++ EpicodusChan/Models/Message.cs
@@ -1,13 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EpicodusChan.Models
 {
   public class Message
   {
-      public int MessageId { get; set; }
-      public string Title { get; set; }
-      public string UserName { get; set; }
-      public string Entry { get; set; }
-      public string Date { get; set; }
-      // public int GroupId { get; set; }
-      public string GroupName { get; set; }
+    public int MessageId { get; set; }
+
+    public int GroupId { get; set; }
+
+    [Required]
+    [StringLength(100, ErrorMessage = "Try to keep your title short!")]
+    public string Title { get; set; }
+    
+    [Required]
+    public string UserName { get; set; }
+
+    [Required]
+    public string Entry { get; set; }
+
+    [Required]
+    public string Date { get; set; }
+    public Group Group { get; set; }
+    
   }
 }
